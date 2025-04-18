46a30948e7e0db8f8b0a8574c7637e6384cfb9b1
vov-java <wolf412@bk.ru>
2025-04-18 19:37:17 +0300

    HM

---
+++ HM class(arythmetyc)/src/Arithmetic.java
@@ -0,0 +1,19 @@
+public class Arithmetic {
+    public int sumNambers (int a,int b){
+        int c = a + b;
+        return c;
+    }
+    public int multyNambers (int a,int b){
+        int c = a * b;
+        return c;
+    }
+    public int maxNamber(int a,int b){
+        int max = (a>b)?a:b;
+        return max;
+    }
+    public int minNamber(int a,int b){
+        int min = (a<b)?a:b;
+        return min;
+    }
+
+}
---
+++ HM class(arythmetyc)/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Arithmetic myAry = new Arithmetic();
+
+        System.out.println("складываю " + myAry.sumNambers(4,2));
+        System.out.println("min = " + myAry.minNamber(6,8));
+    }
+}
