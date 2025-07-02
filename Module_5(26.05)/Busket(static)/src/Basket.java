83eea2d0eef198174102e4f300b41f91469ab352
vov-java <wolf412@bk.ru>
2025-07-02 16:01:33 +0300

    basket

--- Module_5(26.05)/Busket(static)/src/Basket.java
+++ Module_5(26.05)/Busket(static)/src/Basket.java
@@ -1,9 +1,9 @@
 public class Basket {
 
-    public int countGoodsCup;
-    public int totalWeithCup;
-    public int priceProduct;
-    public String productNameCup = "";
+    private int countGoodsCup;
+    private int totalWeithCup;
+    private int priceProduct;
+    private String productNameCup = "";
     private static int allPriceProductAllCup = 0;
     private static int allCountProductAllCup = 0;
     public static int countBasket = 0;
@@ -11,14 +11,16 @@ public class Basket {
     public Basket(){
         countBasket++;
     }
+
     public void appProduct(String product, int countGoods, int weightProduct, int priceProduct) {
-        this.productNameCup = productNameCup.concat(product);
+        this.productNameCup = productNameCup + product;
         this.totalWeithCup = totalWeithCup + (countGoods * weightProduct);
         this.countGoodsCup = countGoodsCup + countGoods;
-        Basket.allCountProductAllCup = allCountProductAllCup + countGoods;
-        Basket.allPriceProductAllCup = allPriceProductAllCup + (countGoods * priceProduct);
+
+       this.allCountProductAllCup = allCountProductAllCup + countGoods ;
+       this.allPriceProductAllCup = allPriceProductAllCup + (countGoods * priceProduct);
     }
-    public static int getAveragePriceProduct(){
+    public static double getAveragePriceProduct(){
        return allPriceProductAllCup / allCountProductAllCup;
     }
     public static int getAllPriceProductAllCup(){
